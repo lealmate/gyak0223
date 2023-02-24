e9e3544428949b886bd1ebc409e1ce5375facdd1
Leo <>
2023-02-24 09:15:14 +0100

    Project base

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.Add(3,4));
+    }
+
+    public int Add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,36 @@
+import io.qameta.allure.*;
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Epic("Epic 1")
+public class MainTest {
+
+    @Test
+    @Story("User Story 1")
+    @Description("Description created to testAdd test")
+    @Severity(SeverityLevel.NORMAL)
+    @Tag("Tag1")
+    @Link("https://example.org")
+    public void testAdd(){
+        Main main = createCalc();
+
+        Assertions.assertEquals(5, main.Add(2, 3));
+    }
+
+    private Main createCalc(){
+        return new Main();
+    }
+
+    @Test
+    @Story("User Story 2")
+    @Description("Description created to testAddWithNegative test")
+    @Severity(SeverityLevel.BLOCKER)
+    @Tag("Tag2")
+    public void testAddWithNegative(){
+        Main main = new Main();
+
+        Assertions.assertEquals(-5, main.Add(-2, -3));
+    }
+}
